05aa707b8403f5d131622045fcc29f0da440f487
Kristian Friis <friis1212@gmail.com>
2024-12-01 22:59:04 +0100

    added a more interactive dungeon

---
+++ BlazorBattle.Client/Modules/DungeonModule/Models/GameTile.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorBattle.Client.Modules.DungeonModule.Models;
+
+public enum TileTypes
+{
+    Grass,
+    Stone,
+    Wall,
+    Treasure,
+    Void
+}
+
+public class GameTile
+{
+    public TileTypes Type { get; set; }
+    public bool IsPassable => Type != TileTypes.Wall;
+
+    public GameTile(TileTypes type)
+    {
+        Type = type;
+    }
+
+    public override string ToString()
+    {
+        return Type.ToString().ToLower();
+    }
+}
